10a7733e1933ac4ba4dd16b08567fe96ee590c90
BVK Ratnesh <ratneshbvk2504@gmail.com>
2025-12-28 14:02:52 +0530

    Defining theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#1A1A2E", // Deep Navy
+        text: "#6B728E", // Muted Gray
+        correct: "#E94560", // Vibrant Pink/Red
+        incorrect: "#00D1FF", // Electric Blue
+        accent: "#FFEE63", // Cyber Yellow
+      },
+      fontFamily: {
+        mono: ["'JetBrains Mono'", "monospace"], // Set default monospaced font
+      },
+    },
   },
   plugins: [],
 };

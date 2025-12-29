1754c82ae4388fc9567124cca81f39de98d3b639
BVK Ratnesh <ratneshbvk2504@gmail.com>
2025-12-29 19:31:25 +0530

    Adding backend URL

--- src/socket.ts
+++ src/socket.ts
@@ -1,3 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000"); // backend URL
+export const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  transports: ["websocket"],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+});
